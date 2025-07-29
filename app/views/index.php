62daccf3062421185584484922bb6180d9ed903a
NAM TOLA <namtola4444@gmail.com>
2025-07-29 23:56:43 +0700

    first commit

---
+++ app/views/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <!-- link bootstrap -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr"
+      crossorigin="anonymous" />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+      crossorigin="anonymous"></script>
+    <!-- link css -->
+    <link rel="stylesheet" href="../assets/styles/style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" />
+  </head>
+  <body class="font-fira">
+    <div class="container-fuid p-0 d-flex">
+      <aside class="col-2 bg-success sticky-top" style="height: 100vh">
+        <div class="w-100 text-center py-3 border-bottom border-2 border-white">
+          <img src="../assets/images/logo.png" alt="no image" class="w-25" />
+          <h1 class="text-light mb-0">
+            piza <span class="text-warning"> Khmer</span>
+          </h1>
+          <p class="text-light">Yummy with me</p>
+        </div>
+        <ul class="list-unstyled text-light mt-3 ps-5">
+          <a href="" class="text-decoration-none text-light">
+            <li class="py-2">
+              <i class="bi bi-house"></i>
+              Home
+            </li>
+          </a>
+          <a href="" class="text-decoration-none text-light">
+            <li class="py-2">
+              <i class="bi bi-house"></i>
+              Items
+            </li>
+          </a>
+          <a href="" class="text-decoration-none text-light">
+            <li class="py-2">
+              <i class="bi bi-house"></i>
+              category
+            </li>
+          </a>
+          <a href="" class="text-decoration-none text-light">
+            <li class="py-2">
+              <i class="bi bi-house"></i>
+              Orders
+            </li>
+          </a>
+        </ul>
+        <div
+          class="px-2 position-absolute bottom-0 w-100 text-center py-3 border-top border-2 border-white">
+          <button class="btn btn-danger w-100 rounded-lg">logout</button>
+        </div>
+      </aside>
+      <main>
+        <h1 class="text-center text-danger text-sm">welcome admin</h1>
+        <header></header>
+        <section></section>
+      </main>
+    </div>
+  </body>
+</html>
